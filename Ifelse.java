f0f03c2da29393441ac7e237db30cf5a619e27bf
Monika145 <92775381+Monikaveni145@users.noreply.github.com>
2022-04-07 19:58:47 +0530

    ifelse program

---
+++ Ifelse.java
@@ -0,0 +1,11 @@
+package tamilnadu.chennai;
+
+public class Ifelse {
+public static void main(String[] args) {
+	int m = 100;
+	if (m<100)
+		System.out.println("goo");
+	else
+		System.out.println("don't go");
+}
+}
